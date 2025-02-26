728a417d65090cba2352bdd184ba27e970114514
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2025-02-26 22:22:38 +0530

    Declare Arraylist

---
+++ Java_Streams/arraylist.java
@@ -0,0 +1,10 @@
+package Java_Streams;
+
+import java.util.ArrayList;
+
+public class arraylist {
+    void list1(){
+        System.out.println("ArrayList");
+        ArrayList<Integer> list = new ArrayList<Integer>(); 
+    }
+}
