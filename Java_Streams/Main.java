8bcc801488da3e88c6179b1a83239525a9baaf12
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2025-02-26 22:31:22 +0530

    Creating an instance of the class && Calling the method to demonstrate ArrayList usage

--- Java_Streams/Main.java
+++ Java_Streams/Main.java
@@ -3,7 +3,11 @@ package Java_Streams;
 import java.util.ArrayList;
 
 public class Main{
-            public static void main(String[] args) {
-                
-            }
+    public static void main(String[] args) {
+        // Creating an instance of the class
+        ArrayListExample example = new ArrayListExample();
+        
+        // Calling the method to demonstrate ArrayList usage
+        example.demonstrateArrayList();
+    }
 }
