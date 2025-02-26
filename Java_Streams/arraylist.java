41497b1bd7091de74df84f0ef44668239471f881
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2025-02-26 22:23:05 +0530

    Add a value to  Arraylist

--- Java_Streams/arraylist.java
+++ Java_Streams/arraylist.java
@@ -6,5 +6,11 @@ public class arraylist {
     void list1(){
         System.out.println("ArrayList");
         ArrayList<Integer> list = new ArrayList<Integer>(); 
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
     }
 }
