300d735a19f9fc5b19b572f1925887299d6c98e0
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2025-02-26 22:28:03 +0530

    Add some methods

--- Java_Streams/ArrayListExample.java
+++ Java_Streams/ArrayListExample.java
@@ -29,5 +29,29 @@ public class ArrayListExample {  // Renamed class to follow Java naming conventi
         
         // Printing the ArrayList contents
         System.out.println("List Elements: " + list);
+
+                // Printing the ArrayList contents
+                System.out.println("List Elements: " + list);
+        
+                // Accessing an element at index 2
+                System.out.println("Element at index 2: " + list.get(2));
+                
+                // Updating an element at index 3
+                list.set(3, 100);
+                System.out.println("Updated List: " + list);
+                
+                // Removing an element at index 1
+                list.remove(1);
+                System.out.println("List after removal: " + list);
+                
+                // Checking if an element exists
+                System.out.println("List contains 50: " + list.contains(50));
+                
+                // Getting the size of the ArrayList
+                System.out.println("Size of the list: " + list.size());
+                
+                // Clearing all elements from the ArrayList
+                list.clear();
+                System.out.println("List after clearing: " + list);
     }
 }
