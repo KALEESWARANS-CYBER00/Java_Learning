4f840f1481076d521027f2dd61f178902ae9d55e
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2025-02-28 21:39:14 +0530

    Declare Linked List with Arrays.asList Methods

---
+++ Java_Streams/LinkedListExample.java
@@ -0,0 +1,9 @@
+package Java_Streams;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class LinkedListExample {
+    LinkedList<Integer> linked=new LinkedList<Integer>(Arrays.asList(10,20,30,40,50,60,1,2,3));
+    
+}
